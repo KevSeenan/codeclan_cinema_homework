2d1e2e5f9cc1a5c08ef4f31129b0320acec2c0f5
KevSeenan <kev.seenan@gmail.com>
2019-08-02 16:06:36 +0100

    Adds find film by id

--- models/films.rb
+++ models/films.rb
@@ -18,4 +18,19 @@ class Film
     @id = film['id'].to_i()
   end
 
+  def self.all()
+    sql = "SELECT * FROM films"
+    films = SqlRunner.run(sql)
+    result = films.map{|film| Film.new(film)}
+    return result
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM films WHERE id = $1"
+    values = [id]
+    result = SqlRunner.run(sql, values)
+    film = self.new(result.first)
+    return film
+  end
+
 end
