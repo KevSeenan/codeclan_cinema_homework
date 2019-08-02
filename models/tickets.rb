cc0e0a02fb4fed86b20ad84d484185907fbf7530
KevSeenan <kev.seenan@gmail.com>
2019-08-02 20:33:57 +0100

    Adds delete ticket by id

--- models/tickets.rb
+++ models/tickets.rb
@@ -37,4 +37,10 @@ class Ticket
     SqlRunner.run(sql, values)
   end
 
+  def delete()
+    sql = "DELETE FROM tickets WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
 end
